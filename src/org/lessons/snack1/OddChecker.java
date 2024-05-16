fdc35572eb53f8836f5b643ef9bae5a0c2f761e6
Amarildo <you@example.com>
2024-05-16 15:21:15 +0200

    complited first snack

--- src/org/lessons/snack1/OddChecker.java
+++ src/org/lessons/snack1/OddChecker.java
@@ -1,4 +1,24 @@
 package org.lessons.snack1;
 
+import java.util.Scanner;
+
 public class OddChecker {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        //Richiesta di inserire un num
+        System.out.print("inserisi un numero: ");
+        int num = scanner.nextInt();
+
+        //verifica se il num e pari o dispari
+        if (num % 2 == 0) {
+            System.out.println("il num e pari");
+        } else {
+            System.out.println("il num e dispari, il num successivo e' "
+            + (num+1));
+        }
+
+        scanner.close();
+    }
 }
