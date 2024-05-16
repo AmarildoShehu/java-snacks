fa70e963836d5fec20cfb8909545b3e87580e2f1
Amarildo <you@example.com>
2024-05-16 15:48:42 +0200

    created array with name and surname, added random and created log for random name generetor

--- src/org/lessons/snack2/RandomNameGenerator.java
+++ src/org/lessons/snack2/RandomNameGenerator.java
@@ -1,7 +1,25 @@
 package org.lessons.snack2;
 
+import java.util.Random;
+
 public class RandomNameGenerator {
     public static void main(String[] args) {
 
+        String[] guestName = { "Amarildo","John","Steven","Brandon","Dio" };
+        String[] guestSurname = {"Shehu","Cena","Gerrard","Bo","Steve"};
+
+        //geeratore num casuali
+        Random random = new Random();
+
+        //num inviti
+        int numberOfGuest = 10;
+        System.out.println("lista degli invitati");
+
+        //logica inviti falsi
+        for (int i = 0; i < numberOfGuest; i++){
+            String randomName = guestName[random.nextInt(guestName.length)];
+            String randomSurname = guestSurname[random.nextInt(guestSurname.length)];
+            System.out.println(randomName +" "+randomSurname);
+        }
     }
 }
