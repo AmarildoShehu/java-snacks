881a7dcc7dcc9d09926cd15d38ef63719e8462d4
Amarildo <you@example.com>
2024-05-16 16:32:46 +0200

    added array number new variable and logic for sum elements in odd position

--- src/org/lessons/snack3/SommaPosizioneDispari.java
+++ src/org/lessons/snack3/SommaPosizioneDispari.java
@@ -3,5 +3,19 @@ package org.lessons.snack3;
 public class SommaPosizioneDispari {
     public static void main(String[] args) {
 
+        //array num
+        int[] numbers = {1,2,3,4,5,6,7,8,9,11,10};
+
+        //variabile per la somma
+        int sommaPosizioneDispari = 0;
+
+        //calcolo somma degli elementi in posizione dispari
+        for (int i = 0; i < numbers.length; i++) {
+            if (i % 2 != 0) {
+                sommaPosizioneDispari += numbers[i];
+            }
+        }
+
+        System.out.println("La somma degli elemnti e': " + sommaPosizioneDispari);
     }
 }
