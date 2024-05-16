73e546e34e88bdd0fb99944a72bbd3db492aa44e
Amarildo <you@example.com>
2024-05-16 17:43:36 +0200

    created a character counter

--- src/org/lessons/snack5/ContatoreCaratteri.java
+++ src/org/lessons/snack5/ContatoreCaratteri.java
@@ -1,7 +1,45 @@
 package org.lessons.snack5;
 
+import java.util.Scanner;
+
 public class ContatoreCaratteri {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Loop
+        while(true) {
+            //richiesta all'utente
+            System.out.print("Inserisci una stringa o 0 per terminare: ");
+            String input = scanner.nextLine();
+
+            //controllo
+            if (input.equals("0")) {
+                System.out.println("Terminato");
+                break;
+            }
+
+            //conteggio dei caratteri
+            int alphabeticCount = 0;
+            int numericCount = 0;
+            int nonAlphanumericCount = 0;
+
+            for (int i=0; i < input.length(); i++){
+                char carattere = input.charAt(i);
+                if (Character.isLetter(carattere)){
+                    alphabeticCount++;
+                } else if (Character.isDigit(carattere)) {
+                    numericCount++;
+                } else {
+                    nonAlphanumericCount++;
+                }
+            }
+
+            //stammpa risultati
+
+            System.out.println("Caratteri alfabetici: " +alphabeticCount);
+            System.out.println("Caratteri numerici: " +numericCount);
+            System.out.println("Simboli: " +nonAlphanumericCount);
+        }
 
     }
 }
